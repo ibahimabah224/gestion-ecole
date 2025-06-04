9f25df90c17f21792131e21ae2740e244e6744db
ibrahimabah224 <ibpita1999@gmail.com>
2025-06-04 18:37:59 +0000

    viewLogin

---
+++ app/src/main/java/com/controller/LoginController.java
@@ -0,0 +1,15 @@
+package com.controller;
+
+import javafx.fxml.FXML;
+// import javafx.scene.control.Label;
+// import javafx.scene.control.TextField;
+
+public class LoginController {
+
+    // Initialize method is called after FXML fields are injected
+    @FXML
+    public void initialize() {
+        // Initialization logic here
+    }
+
+}
--- app/src/main/java/com/gestionecole/App.java
+++ app/src/main/java/com/gestionecole/App.java
@@ -1,17 +1,22 @@
 package com.gestionecole;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class App extends Application {
-     @Override
-    public void start(Stage stage) {
-        Label label = new Label("Bonjour JavaFX !");
-        Scene scene = new Scene(label, 400, 200);
+    @Override
+    public void start(Stage stage) throws Exception {
+        // integration de la vue login.fxml
+        Parent root = FXMLLoader.load(getClass().getResource("/view/fxml/login.fxml"));
+        Scene scene = new Scene(root);
+        // Intégration du fichier style.css
+        scene.getStylesheets().add(getClass().getResource("/view/css/style.css").toExternalForm());
         stage.setScene(scene);
-        stage.setTitle("Demo JavaFX");
+        stage.setTitle("APPLICATION DE GESTION DE MON ECOLE");
+        stage.setResizable(false);
         stage.show();
     }
 
